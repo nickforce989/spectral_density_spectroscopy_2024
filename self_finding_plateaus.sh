f014ff6f110dd07144c393ae198f91a790ed0ba7
nickforce989 <nic.forz@gmail.com>
2024-04-29 15:35:12 +0100

    Issue #13

--- self_finding_plateaus.sh
+++ self_finding_plateaus.sh
@@ -7,16 +7,16 @@ cd plateaus
 #python record_meson_AS.py
 #python analysis_AS.py
 
-#python record_GEVP_F.py
+python record_GEVP_F.py
 python analysis_GEVP_F.py
 
-#python record_GEVP_AS.py
+python record_GEVP_AS.py
 python analysis_GEVP_AS.py
 
-#python record_GEVP_mixF.py
+python record_GEVP_mixF.py
 python analysis_GEVP_MIX_F.py
 
-#python record_GEVP_mixAS.py
+python record_GEVP_mixAS.py
 python analysis_GEVP_MIX_AS.py
 
 python plot_GEVP_F.py
