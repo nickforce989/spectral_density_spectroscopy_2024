43ce83be7151ec1cb2eab1237d703b275ea6c440
nickforce989 <nic.forz@gmail.com>
2024-05-17 15:11:53 +0100

    Correct Latex check

--- just_plotting/code/topologies/main.sh
+++ just_plotting/code/topologies/main.sh
@@ -2,7 +2,12 @@ cd ./flow_analysis/
 julia main_flow.jl
 cd ../MadrasSokal/
 julia "scripts/table.jl"
-julia "scripts/plaquette.jl"
-julia "scripts/topological_charge.jl"
+if command -v latex > /dev/null 2>&1; then
+    echo "LaTeX is installed."
+    julia "scripts/plaquette.jl"
+    julia "scripts/topological_charge.jl"  
+else
+    echo "LaTeX is not installed."  
+fi
 cd ..
 
