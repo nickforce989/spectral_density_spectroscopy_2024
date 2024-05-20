dad8c45f84234392e7746f05ef8e8819e980acc9
nickforce989 <nic.forz@gmail.com>
2024-05-20 16:07:01 +0100

    Latex check, full workflow or not

--- reproduce_everything.sh
+++ reproduce_everything.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-bash run_plateaus.sh
-bash run_spectral_densities.sh
-python CSVs_to_tables.py
-bash run_plots.sh
+
+# Check if LaTeX is installed
+if ! command -v latex > /dev/null 2>&1; then
+    echo "LaTeX is not installed. Producing CSVs, no plots nor tables.tex files"
+    #bash run_plateaus.sh
+    bash run_spectral_densities.sh
+    
+else
+    echo "Latex is installed. Running full workflow."
+    #bash run_plateaus.sh
+    bash run_spectral_densities.sh
+    python CSVs_to_tables.py
+    bash run_plots.sh
+fi
