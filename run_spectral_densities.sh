ba58d0824f440a7e838902ed34931ca0d8a86048
nickforce989 <nic.forz@gmail.com>
2024-05-27 10:55:36 +0100

    Minor

--- run_spectral_densities.sh
+++ run_spectral_densities.sh
@@ -3,7 +3,8 @@
 bash check_latex.sh
 
 directory="input_fit"
-subdirs=("M1" "M2" "M3" "M4" "M5")
+#subdirs=("M1" "M2" "M3" "M4" "M5")
+subdirs=("M1")
 all_subdirs_present=true
 
 for subdir in "${subdirs[@]}"; do
@@ -16,9 +17,27 @@ done
 cd lsd_out
 
 if [ "$all_subdirs_present" = false ]; then
-    python analyse_data.py
-    python print_samples.py
-    python fit_data.py
+    inner_condition_met=true
+    for subdir in "${subdirs[@]}"; do
+        echo "Checking subdirectories for pattern: ${subdir}_*"
+        count=$(find . -maxdepth 1 -type d -name "${subdir}_*" | wc -l)
+        echo "Found $count subdirectories matching ${subdir}_*"
+        if [ "$count" -ne 24 ]; then
+            inner_condition_met=false
+            break
+        fi
+    done
+	
+
+    if [ "$inner_condition_met" = false ]; then
+        python analyse_data.py
+        python print_samples.py
+        python fit_data.py
+    else
+        python print_samples.py
+        python fit_data.py
+    fi
+
 else
     python fit_data.py
 fi
